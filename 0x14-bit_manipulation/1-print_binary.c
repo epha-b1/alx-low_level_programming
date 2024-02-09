1e377c573d1efc7a1241f485ae628d99506b10cf
epha-b1 <ephremyonatan27@gmail.com>
2024-02-09 22:55:33 +0300

    fix efficiency

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,12 +2,12 @@
 
 /**
  * print_binary - Prints the binary representation of a number.
- * @n: The number to be converted to binary and printed.
+ * @n: The number to print in binary.
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+    if (n > 1)
+        print_binary(n >> 1);
 
-	_putchar((n & 1) + '0');
+    _putchar((n & 1) + '0');
 }
