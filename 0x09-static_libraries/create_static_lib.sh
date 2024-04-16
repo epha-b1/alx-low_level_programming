c4eeec157342da015de85c4e36cf476bda2837ba
Ephrem B <ephremyonatan27@gmail.com>
2024-04-16 21:45:53 +0300

    Update create\_static\_lib.sh: Adds extra warnings, removes unnecessary object files, and runs ranlib
    * Adds `-Wextra` and `-Werror` to gcc flags for extra warnings and treat warnings

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -c *.c
-
-# Create the static library
-ar rcs liball.a *.o
-
-# Clean up object files
-rm *.o
-
-echo "Static library liball.a created successfully!"
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
